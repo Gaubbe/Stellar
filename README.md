b10c3c21f9638beab32b580b93cc9fbd16b1b15b
GabrielAube <gabriel12125@gmail.com>
2021-09-04 20:56:16 -0400

    Initial project configuration

---
+++ README.md
@@ -0,0 +1,5 @@
+# Stellar
+A Hollow Knight pathfinding bot
+
+## Building
+To build this mod, you first need to copy your Hollow Knight "Managed" with the modding api installed to the folder with the project file. This mod uses the [hk-modding-api version 1.5.75.11827-63](https://github.com/hk-modding/api/releases/tag/1.5.75.11827-63).
---
+++ Stellar/Stellar.cs
@@ -0,0 +1,20 @@
+﻿using Modding;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Stellar
+{
+    public class Stellar : Mod
+    {
+        internal static Stellar Instance;
+
+        public Stellar() : base("Stellar") { }
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            Instance = this;
+
+            Log("poggers");
+        }
+    }
+}
