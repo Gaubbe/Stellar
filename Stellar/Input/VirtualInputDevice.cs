fe7d35b7b295b5e5996cc038f9fac750861c80ce
Gaubbe <gabriel12125@gmail.com>
2021-09-04 21:21:20 -0400

    Added directory structure for Input namespace

---
+++ Stellar/Input/VirtualInputDevice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stellar.Input
+{
+    public class VirtualInputDevice
+    {
+        public bool Dash { get; set; }
+    }
+}
