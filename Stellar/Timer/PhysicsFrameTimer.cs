b32d3d28c34180ffb9faf2f4465deaf4dbc53e38
Gaubbe <gabriel12125@gmail.com>
2021-09-10 19:27:38 -0400

    Added PhysicsFrameTimer class

---
+++ Stellar/Timer/PhysicsFrameTimer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stellar.Timer
+{
+    public class PhysicsFrameTimer
+    {
+        public int FrameCount { get; private set; }
+    
+        public PhysicsFrameTimer(int initialFrameCount)
+        {
+            FrameCount = initialFrameCount;
+        }
+
+        public PhysicsFrameTimer() : this(0) { }
+
+        public void OnPhysicsFrame()
+        {
+            FrameCount++;
+        }
+
+        public void Reset()
+        {
+            FrameCount = 0;
+        }
+    }
+}
